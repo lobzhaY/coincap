ea6a16833cc829c67daeeb8e5df6dfa33d70c1bb
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-26 23:02:16 +0300

    add a call to the useGetCoincapQuery hook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import { Flex, Col, Row, Typography } from "antd";
 import CurrenciesItem from "./CurrenciesItem/CurrenciesItem";
+import { useGetCoincapQuery } from "../../redux/coincap";
+import IResponseItem from "../../types/responseItem";
 import styles from "./Header.module.css";
 
 function Header() {
     const { Title } = Typography;
+    const { data = [], isLoading } = useGetCoincapQuery();
+    const [three, setThree] = useState([]);
+    useEffect(() => {
+        if (data.data) {
+            const firstThree = data.data.slice(0, 3);
+            setThree(firstThree);
+        }
+    }, [data]);
 
     return (
         <header className={styles.header__container}>
@@ -23,9 +34,17 @@ function Header() {
                         </Col>
                     </Row>
                     <Row>
-                        <CurrenciesItem />
-                        <CurrenciesItem />
-                        <CurrenciesItem />
+                        {isLoading ? (
+                            <p>LOADING</p>
+                        ) : (
+                            three.map((elem: IResponseItem) => (
+                                <CurrenciesItem
+                                    elemName={elem.name}
+                                    elemPrice={elem.priceUsd}
+                                    key={elem.id}
+                                />
+                            ))
+                        )}
                     </Row>
                 </div>
                 <div className={styles.header_total}>
