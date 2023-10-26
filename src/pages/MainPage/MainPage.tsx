303c06f3b864ac11e908d3bc60ce3e750364adea
Yuliya Lobzha <yulia.lobzha@gmail.com>
2023-10-26 22:59:02 +0300

    add a call to the useGetCoincapQuery hook

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,18 +1,34 @@
+import { useEffect, useState } from "react";
 import { Pagination } from "antd";
 import TableComponent from "../../components/Table/Table";
+import { useGetCoincapQuery } from "../../redux/coincap";
 import styles from "./MainPage.module.css";
 
 function MainPage() {
+    const { data = [], isLoading } = useGetCoincapQuery();
+    const [ten, setTen] = useState([]);
+    useEffect(() => {
+        if (data.data) {
+            const firstten = data.data.slice(0, 10);
+            setTen(firstten);
+        }
+    }, [data]);
     return (
         <main>
-            <TableComponent />
-            <div className={styles.pagination_container}>
-                <Pagination
-                    defaultCurrent={1}
-                    total={50}
-                    className={styles.pagination}
-                />
-            </div>
+            {isLoading ? (
+                <p>LOADING</p>
+            ) : (
+                <>
+                    <TableComponent dataTen={ten} />
+                    <div className={styles.pagination_container}>
+                        <Pagination
+                            defaultCurrent={1}
+                            total={50}
+                            className={styles.pagination}
+                        />
+                    </div>
+                </>
+            )}
         </main>
     );
 }
