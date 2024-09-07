83708af3bff2716c48825e5f88918c6388379fd1
Yuliya Lobzha <yulia.lobzha@gmail.com>
2024-09-07 19:54:50 +0300

    feat: add base header layout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,32 @@
+import { ShoppingOutlined } from '@ant-design/icons';
+
+import { PopularCoin } from './popular-coin';
+
+import styles from './header.module.scss';
+
 export const Header: React.FC = () => {
   return (
-    <h1>Header</h1>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.popularCoinContainer}>
+        <h3>Популярные криптовалюты</h3>
+        <div className={styles.coinList}>
+          <PopularCoin />
+          <PopularCoin />
+          <PopularCoin />
+        </div>
+      </div>
+
+      <div className={styles.shoppingContainer}>
+        <ShoppingOutlined className={styles.soppingCartIcon} />
+        <div className={styles.shoppingCart}>
+          <h3>Итого</h3>
+          <h2>134,32 USD</h2>
+          <div className={styles.shoppingCartStatistic}>
+            <span>+2,38</span>
+            <span>(1,80 %)</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/header/popular-coin/index.ts
@@ -0,0 +1,1 @@
+export { PopularCoin } from './popular-coin';
