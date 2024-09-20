fd01a195bf4176122a1d5c48e65ef44a0f9ad3fb
Yuliya Lobzha <yulia.lobzha@gmail.com>
2024-09-20 20:27:25 +0300

    fix: remove unnecessary styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Suspense, useEffect } from "react";
+
 import { Outlet, useNavigate } from "react-router-dom";
 
-import { Header, Loader } from "./components";
-import { Suspense, useEffect } from "react";
 import { useAppSelector } from "./hooks";
+
+import { Header, Loader } from "./components";
+
+import styles from "./App.module.scss";
 
 function App() {
   const { isError } = useAppSelector((state) => state.app);
@@ -10,19 +14,19 @@ function App() {
 
   useEffect(() => {
     if (isError) {
-     navigate('/404', {replace: true});
+      navigate("/404", { replace: true });
     }
   }, [isError, navigate]);
-  
+
   return (
-    <>
+    <div className={styles.root}>
       <Suspense fallback={<Loader />}>
         <Header />
         <div>
           <Outlet />
         </div>
       </Suspense>
-    </>
+    </div>
   );
 }
 
